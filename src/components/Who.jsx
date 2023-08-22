c608954ba49dc783df183adec23f05a0613de3bd
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-08-22 14:47:24 +0200

    smooth scrolling added

---
+++ src/components/Who.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  height: 100vh;
+  background-color: #b49ccc;
+  scroll-snap-align: center;
+`
+
+const Who = () => {
+  return (
+    <Section>Who</Section>
+  )
+}
+
+export default Who
