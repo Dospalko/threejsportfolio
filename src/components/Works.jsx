8f8c8d87ffca09397187de106faea47d4e2f9448
Dominik Paľo <dominik.palo@student.tuke.sk>
2023-08-23 22:08:01 +0200

    rotating 3D models added wow

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,40 +1,50 @@
-import React from 'react'
-import styled from 'styled-components'
-
-
-const data =  [
-  "Web development",
-  "Testing",
-  "Programming",
-  "Social Media",
-  "Design"
-];
+import React, { useState } from "react";
+import styled from "styled-components";
+import WebDev from "./WebDev";
+import Testing from "./Testing";
+import Programming from "./Programming";
+const data = ["Web development", "Testing", "Programming"];
 
 const Section = styled.div`
   height: 100vh;
+  scroll-snap-align: center;
   display: flex;
   justify-content: center;
-  scroll-snap-align: center;
-`
+  position: relative;
+  color: black;
+  font-size: 14px;
+  font-weight: 300;
+`;
+
 const Container = styled.div`
   width: 1400px;
   display: flex;
   justify-content: space-between;
-  scroll-snap-align: center;
-`
+
+  @media only screen and (max-width: 768px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`;
 
 const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-  
-`
+
+  @media only screen and (max-width: 768px) {
+    padding: 20px;
+    justify-content: center;
+  }
+`;
+
 const List = styled.ul`
   list-style: none;
   display: flex;
   flex-direction: column;
   gap: 20px;
-`
+`;
+
 const ListItem = styled.li`
   font-size: 80px;
   font-weight: bold;
@@ -43,53 +53,66 @@ const ListItem = styled.li`
   -webkit-text-stroke: 1px white;
   position: relative;
 
+  @media only screen and (max-width: 768px) {
+    font-size: 24px;
+    color: white;
+    -webkit-text-stroke: 0px;
+  }
+ 
+
 
   &:after {
-    content: "${(props)=>props.text}";
+    content: "${(props) => props.text}";
     position: absolute;
-    top:0;
-    left:0;
-    color: hsl(186.84563758389262, 65.63876651982379%, 55.490196078431374%);
-    width: 0;
+    top: 0;
+    left: 0;
+    color: pink;
+    width: 0px;
     overflow: hidden;
     white-space: nowrap;
   }
 
-  &:hover{
-    &:after{
+  &:hover {
+    &:after {
       animation: moveText 0.5s linear both;
+
       @keyframes moveText {
-          to{
-              width: 100%;
-          }
+        to {
+          width: 100%;
+        }
       }
     }
   }
- 
 `;
-
 const Right = styled.div`
- flex: 1;
- 
-`
-
-
+  flex: 1;
+`;
 const Works = () => {
+  const [work, setWork] = useState("Web development");
   return (
     <Section>
       <Container>
         <Left>
           <List>
-            {data.map(item=>(
-              <ListItem key={item} text={item}>{item}</ListItem>
+            {data.map((item) => (
+              <ListItem key={item} text={item} onClick={() => setWork(item)}>
+                {item}
+              </ListItem>
             ))}
-            <ListItem></ListItem>
           </List>
         </Left>
-        <Right></Right>
+        <Right>
+          {work === "Web development" ? (
+            <WebDev />
+          ) : work === "Testing" ? (
+            <Testing />
+          ) : (
+            <Programming />
+          )}
+        </Right>
       </Container>
     </Section>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
